8c47fcd48a7fc10f188ad180eddad7026bec7f93
Miree Zeidan <mireezeidan@gmail.com>
2022-11-20 20:24:48 -0600

    added code to js file to show current day

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ $(function () {
     localStorage.setItem(parent, description);
   });
 
+  let date = dayjs().format("MMM D, YYYY");
+  $("#currentDay").text(date);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
