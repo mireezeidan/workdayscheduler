2f965517278a5728404bba6d151597b166e4480d
Miree Zeidan <mireezeidan@gmail.com>
2022-11-21 18:28:45 -0600

    added code to add and remove past, present, and future classes as well as adding items to the hour blocks

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ $(function () {
   let date = dayjs().format("MMM D, YYYY");
   $("#currentDay").text(date);
 
+  $(".time-block").each(function () {
+    $(this).removeClass("future");
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    let hour = parseInt($(this).attr("id").split("-")[1]);
+
+    if (hour > dayjs().hour()) {
+      $(this).addClass("future");
+    } else if (hour === dayjs().hour()) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+  $("#hour-9").children(".description").val(localStorage.getItem("hour-9"));
+  $("#hour-10").children(".description").val(localStorage.getItem("hour-10"));
+  $("#hour-11").children(".description").val(localStorage.getItem("hour-11"));
+  $("#hour-12").children(".description").val(localStorage.getItem("hour-12"));
+  $("#hour-13").children(".description").val(localStorage.getItem("hour-13"));
+  $("#hour-14").children(".description").val(localStorage.getItem("hour-14"));
+  $("#hour-15").children(".description").val(localStorage.getItem("hour-15"));
+  $("#hour-16").children(".description").val(localStorage.getItem("hour-16"));
+  $("#hour-17").children(".description").val(localStorage.getItem("hour-17"));
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
